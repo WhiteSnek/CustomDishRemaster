e8fd59a9a3254fbd65060548b58a86c8c987d50b
WhiteSnake <whitesnake35711@gmail.com>
2025-01-24 22:46:15 +0530

    writing tests for customer service: register tests done

--- services/customer-service/src/config/index.ts
+++ services/customer-service/src/config/index.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDb = async (): Promise<void> => {
     try {
-      await mongoose.connect(`${process.env.MONGODB_URI}/customDish?authSource=admin`);
+      if(!process.env.MONGODB_URI){
+        throw new Error('Mongodb url not defined')
+      }
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log(`MongoDB Connected`);
     } catch (err) {
       console.log("MongoDb connection error: ", err);
