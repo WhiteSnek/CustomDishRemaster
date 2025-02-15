02719f7f38fee38ad8b40fd297ebac48bda8eb64
WhiteSnake <whitesnake35711@gmail.com>
2025-02-15 17:50:54 +0530

    initialized repository

---
+++ services/coupon-service/src/repository/index.ts
@@ -0,0 +1,32 @@
+import { ObjectId } from "mongoose";
+import { Coupon } from "../model";
+
+class Repository {
+  async create(data: any) {
+    return await Coupon.create(data);
+  }
+
+  async findOne(data: any) {
+    return await Coupon.findOne(data);
+  }
+
+  async findById(id: ObjectId) {
+    return await Coupon.findById(id);
+  }
+
+  async update(id: string | ObjectId, data: any) {
+    return await Coupon.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  async claim(id: ObjectId, userId: string) {
+    return await Coupon.findByIdAndUpdate(
+      id,
+      {
+        $push: { claimedBy: userId },
+      },
+      { new: true }
+    );
+  }
+}
+
+export default Repository;
